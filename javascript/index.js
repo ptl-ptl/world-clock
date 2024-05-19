fb9a483875c74317e571e5e9ce1ddce443c0d4ad
ptl-ptl <155249971+ptl-ptl@users.noreply.github.com>
2024-05-19 13:42:25 +0200

    Added Js for single location

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,30 @@
+function updateTime() {
+  //Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  //let losAngelesDateElement = document.querySelector("#los-angeles .date");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do yyyy");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+  /*losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
+  "HH:mm:ss"
+)} <small> ${losAngelesTime.format("A")} </small>`;*/
+
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do yyyy");
+  parisTimeElement.innerHTML = parisTime.format(
+    "HH:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
